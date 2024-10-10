b20dc79d9eda50aa761d74021bb04dbb8acd2ff7
Zergus325 <alex.usenko.97@gmail.com>
2024-10-11 00:44:19 +0300

    bagraund-img

---
+++ src/main.js
@@ -0,0 +1,6 @@
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { fetchImages } from './js/my-project.js';
