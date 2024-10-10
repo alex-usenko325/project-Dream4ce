bfa98ce7f4674620d7a55be0a522f71b991e2247
Zergus325 <alex.usenko.97@gmail.com>
2024-10-11 00:49:29 +0300

    delite simplebox

--- src/main.js
+++ src/main.js
@@ -1,6 +1,1 @@
-// Описаний в документації
-import SimpleLightbox from 'simplelightbox';
-// Додатковий імпорт стилів
-import 'simplelightbox/dist/simple-lightbox.min.css';
-
 import { fetchImages } from './js/my-project.js';
