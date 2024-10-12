41e6859b8077d1ddcf02207698e4c5fe73cf915b
Maryna H <mary_solnechko@yahoo.com>
2024-10-12 19:41:41 +0200

    correction mob styles, js
    correction of html css, add mobile, js for theme change and mobile

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+// Открытие мобильного меню
+
 const menuButton = document.querySelector('[data-menu-button]');
 const closeButton = document.querySelector('[data-menu-button-close]');
 const mobileMenu = document.querySelector('[data-menu]');
@@ -10,3 +12,25 @@ closeButton.addEventListener('click', () => {
   mobileMenu.classList.remove('is-open');
 });
 
+// theme switch header and body
+// Получаем чекбокс по ID
+const themeCheckbox = document.getElementById('theme-checkbox');
+
+if (localStorage.getItem('theme') === 'dark') {
+    document.body.classList.add('dark-theme');
+    document.querySelector('header').classList.add('dark-theme'); 
+    themeCheckbox.checked = true; 
+}
+
+
+themeCheckbox.addEventListener('change', function() {
+    if (themeCheckbox.checked) {
+        document.body.classList.add('dark-theme');
+        document.querySelector('header').classList.add('dark-theme'); 
+        localStorage.setItem('theme', 'dark');
+    } else {
+        document.body.classList.remove('dark-theme');
+        document.querySelector('header').classList.remove('dark-theme'); // Убираем класс у хедера
+        localStorage.setItem('theme', 'light');
+    }
+});
