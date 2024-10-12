5d2cd072e6dadf699943aa817aaab4f5e0e00d1a
Maryna H <mary_solnechko@yahoo.com>
2024-10-12 13:00:44 +0200

    header mob menu

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
+const menuButton = document.querySelector('[data-menu-button]');
+const closeButton = document.querySelector('[data-menu-button-close]');
+const mobileMenu = document.querySelector('[data-menu]');
+
+menuButton.addEventListener('click', () => {
+  mobileMenu.classList.add('is-open');
+});
+
+closeButton.addEventListener('click', () => {
+  mobileMenu.classList.remove('is-open');
+});
 
