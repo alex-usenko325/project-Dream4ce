b33a1a8c69ddda1c1c4714f202f6c137849a808b
Zergus325 <alex.usenko.97@gmail.com>
2024-10-14 02:01:58 +0300

    heder all

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,33 +1,18 @@
 const menuButton = document.querySelector('[data-menu-button]');
 const closeButton = document.querySelector('[data-menu-button-close]');
 const mobileMenu = document.querySelector('[data-menu]');
+const menuLinks = document.querySelectorAll('.mob-nav-link');
 
 menuButton.addEventListener('click', () => {
-mobileMenu.classList.add('is-open');
+  mobileMenu.classList.add('is-open');
 });
 
 closeButton.addEventListener('click', () => {
-mobileMenu.classList.remove('is-open');
+  mobileMenu.classList.remove('is-open');
 });
 
-
-const themeCheckbox = document.getElementById('theme-checkbox');
-
-if (localStorage.getItem('theme') === 'dark') {
-    document.body.classList.add('dark-theme');
-    document.querySelector('header').classList.add('dark-theme'); 
-    themeCheckbox.checked = true; 
-}
-
-
-themeCheckbox.addEventListener('change', function() {
-    if (themeCheckbox.checked) {
-        document.body.classList.add('dark-theme');
-        document.querySelector('header').classList.add('dark-theme'); 
-        localStorage.setItem('theme', 'dark');
-    } else {
-        document.body.classList.remove('dark-theme');
-        document.querySelector('header').classList.remove('dark-theme');
-        localStorage.setItem('theme', 'light');
-    }
+menuLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    mobileMenu.classList.remove('is-open');
+  });
 });
