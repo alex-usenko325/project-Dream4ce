26f9d0a2b15460df3977193fdac89e7f69e874b3
Zergus325 <alex.usenko.97@gmail.com>
2024-10-14 10:58:15 +0300

    dark them

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,7 +1,6 @@
-
-const menuOpenBtn = document.getElementById('menu-open-btn'); 
-const menuCloseBtn = document.querySelector('[data-menu-button-close]'); 
-const mobileMenu = document.querySelector('.mob-menu'); 
+const menuOpenBtn = document.getElementById('menu-open-btn');
+const menuCloseBtn = document.querySelector('[data-menu-button-close]');
+const mobileMenu = document.querySelector('.mob-menu');
 
 function openMenu() {
   mobileMenu.classList.add('is-open');
@@ -17,5 +16,22 @@ menuCloseBtn.addEventListener('click', closeMenu);
 const menuLinks = document.querySelectorAll('.mob-nav-link');
 menuLinks.forEach(link => {
   link.addEventListener('click', closeMenu);
+});
 
+const themeCheckbox = document.getElementById('change-theme');
+if (localStorage.getItem('theme') === 'dark') {
+  document.body.classList.add('dark-theme');
+  document.querySelector('header').classList.add('dark-theme');
+  themeCheckbox.checked = true;
+}
+themeCheckbox.addEventListener('change', function () {
+  if (themeCheckbox.checked) {
+    document.body.classList.add('dark-theme');
+    document.querySelector('header').classList.add('dark-theme');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    document.body.classList.remove('dark-theme');
+    document.querySelector('header').classList.remove('dark-theme');
+    localStorage.setItem('theme', 'light');
+  }
 });
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import '/js/mobile-menu.js';
-// import '/js/dark-theme.js';
+import '/js/dark-theme.js';
 import '/js/work.js';
 import '/js/faq.js';
 import '/js/my-projects.js';
